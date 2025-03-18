3bf0f72d900a7ae188da5e9f74fa61d80a991329
Bilala14 <kanianabilamuntaz14@gmail.com>
2025-03-19 02:02:36 +0700

    tambah beranda

---
+++ resources/views/jadwal/beranda.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <title>Beranda - Jadwal Guru SD</title>
+   
+</head>
+<body class="container mt-5">
+    <h1 class="text-center">Selamat Datang di Sistem Penjadwalan Guru SD</h1>
+    <p class="text-center">Sistem ini digunakan untuk melihat jadwal mengajar guru di sekolah dasar.</p>
+
+    <div class="text-center mt-4">
+        <a href="/jadwal" class="btn btn-primary">Lihat Jadwal Guru</a>
+    </div>
+
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
